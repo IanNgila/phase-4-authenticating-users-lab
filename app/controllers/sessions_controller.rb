f6089b33f1a068fd5c4bfb098abed1501bd4966e
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-10-12 20:43:20 +0300

    commit tree 04248e070a27e4f6797042db83bfa03b42883a8d

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    # create method to login 
+    def create 
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id 
+        render json: user
+    end 
+
+    # destroy  method to logout
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
