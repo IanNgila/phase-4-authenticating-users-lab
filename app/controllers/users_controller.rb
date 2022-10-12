2ae08f68d99c80cc1bfde7b11ca35632d688b517
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-10-12 20:49:29 +0300

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    def show
+        user = User.find(session[:user_id])
+        if user 
+            render json: user 
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+end
